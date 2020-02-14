75499ca5aa719712dac349a283a3221a46911d06
Artem Kozyr <zymber6@yahoo.com>
2020-02-14 21:36:12 +0200

    Done all functions and methods.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -40,68 +40,6 @@ public class Controller {
         }
     }
 
-    public void makeDecryption(ActionEvent actionEvent) throws FileNotFoundException {
-        StringBuilder data = new StringBuilder();
-        keyString = keyfield.getText().toLowerCase();
-        System.out.println(keyString);
-        try {  //always, when you will work with the files, you should use try-catch!
-            char letter;
-            int letterCode;
-
-            Scanner in = new Scanner(selectedFile);  //it started to read from the begging - \n - symbol of the field end
-            //List<String> container = new ArrayList<>();
-
-            while (in.hasNextLine()) {
-                data.append(in.nextLine()); //save to the data all of the line and then go further
-            }
-
-            System.out.println(data);
-
-            char letterASCICode;
-            int letterASCICodeInt;
-
-            int j = 0;
-            for (int i = 0; i < data.length(); i++) {
-                letter = data.charAt(i);
-                letterCode = letter;
-                if ((char) letterCode >= 65 && (char) letterCode <= 90 || (char) letterCode >= 97 && (char) letterCode <= 122) {
-
-                    if (keyString.length() == j) {
-                        j = 0;
-                    }
-                    letterASCICode = keyString.charAt(j);
-
-                    letterASCICodeInt = letterASCICode;//int
-                    letterASCICodeInt -= 97;// shift on -97 in ASCI code
-
-                    letterCode = letterCode - letterASCICodeInt;
-                    //System.out.println(letterASCICodeInt);
-
-                    sizeOfChar = checkUpperCase(letter);
-                    if (sizeOfChar) {
-                        if (letterCode < 65) {
-                            letterCode += 26;//shift the digit
-                        }
-                    } else {
-                        if (letterCode < 97) {
-                            letterCode += 26; //shift the digit
-                        }
-                    }
-                    letter = (char) letterCode;
-                    j++;
-                }
-                newData = newData + letter;
-            }
-            System.out.println(newData);
-            //writeFile(newData);
-            newData = "";
-            in.close(); //when we finished to work with the file, you should close it down!
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(); //write the details about the error to error's window
-        }
-    }
-
     public void makeEcryption(ActionEvent actionEvent) throws IOException {
         StringBuilder data = new StringBuilder();
         try {  //always, when you will work with the files, you should use try-catch!
@@ -165,6 +103,68 @@ public class Controller {
         }
     }
 
+    public void makeDecryption(ActionEvent actionEvent) throws FileNotFoundException {
+        StringBuilder data = new StringBuilder();
+        keyString = keyfield.getText().toLowerCase();
+        System.out.println(keyString);
+        try {  //always, when you will work with the files, you should use try-catch!
+            char letter;
+            int letterCode;
+
+            Scanner in = new Scanner(selectedFile);  //it started to read from the begging - \n - symbol of the field end
+            //List<String> container = new ArrayList<>();
+
+            while (in.hasNextLine()) {
+                data.append(in.nextLine()); //save to the data all of the line and then go further
+            }
+
+            System.out.println(data);
+
+            char letterASCICode;
+            int letterASCICodeInt;
+
+            int j = 0;
+            for (int i = 0; i < data.length(); i++) {
+                letter = data.charAt(i);
+                letterCode = letter;
+                if ((char) letterCode >= 65 && (char) letterCode <= 90 || (char) letterCode >= 97 && (char) letterCode <= 122) {
+
+                    if (keyString.length() == j) {
+                        j = 0;
+                    }
+                    letterASCICode = keyString.charAt(j);
+
+                    letterASCICodeInt = letterASCICode;//int
+                    letterASCICodeInt -= 97;// shift on -97 in ASCI code
+
+                    letterCode = letterCode - letterASCICodeInt;
+                    //System.out.println(letterASCICodeInt);
+
+                    sizeOfChar = checkUpperCase(letter);
+                    if (sizeOfChar) {
+                        if (letterCode < 65) {
+                            letterCode += 26;//shift the digit
+                        }
+                    } else {
+                        if (letterCode < 97) {
+                            letterCode += 26; //shift the digit
+                        }
+                    }
+                    letter = (char) letterCode;
+                    j++;
+                }
+                newData = newData + letter;
+            }
+            System.out.println(newData);
+            //writeFile(newData);  //if it's need to write a new decrypted version
+            newData = "";
+            in.close(); //when we finished to work with the file, you should close it down!
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); //write the details about the error to error's window
+        }
+    }
+
     public boolean checkUpperCase(char letter) {
         return (int) letter >= 65 && (int) letter <= 90;
     }
@@ -177,11 +177,9 @@ public class Controller {
     }
 
     public String getKey(ActionEvent actionEvent) {
-
         if (!keyfield.getText().equals("") && selectedFile != null){
             ecptBtn.setDisable(false);
             decptBtn.setDisable(false);
         }
-        return keyfield.getText();
     }
 }
