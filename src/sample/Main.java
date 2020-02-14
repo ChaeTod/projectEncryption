7d930757347ad549b3b4745cf9b7f3fd509d62fb
Artem Kozyr <zymber6@yahoo.com>
2020-02-14 21:26:31 +0200

    Done all functions and methods.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser;
 
-import java.io.File;
+import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -17,22 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Encryption");
-
-
-        //FileChooser fileChooser = new FileChooser();
-
-        //Button button = new Button("Select File");
-        //button.setOnAction(e -> {
-        //    File selectedFile = fileChooser.showOpenDialog(primaryStage);
-        //});
-
-        //VBox vBox = new VBox(button);
-        //Scene scene = new Scene(vBox, 960, 600);
-
-
         primaryStage.setScene(new Scene(root, 390, 200));
-
-        //primaryStage.setScene(scene);
         primaryStage.show();
     }
 
